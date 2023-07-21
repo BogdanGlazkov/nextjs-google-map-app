19742c40f026be4ef865a5a23a0c64d3e526e9a0
BogdanGlazkov <bglazkov@i.ua>
2023-07-21 21:24:29 +0300

    Add Home page layout

--- pages/index.js
+++ pages/index.js
@@ -6,8 +6,12 @@ const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <main>
+    <main className="flex">
       <SideNavBar />
+      <section className="grid grid-cols-1 md:grid-cols-2 px-6 md:px-10 w-full mt-10">
+        <div> Business List</div>
+        <div>Google Map</div>
+      </section>
     </main>
   );
 }
