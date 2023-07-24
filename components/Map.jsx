f52e8c5ae05480d6a9eca79492b3fdb3c835f1f3
BogdanGlazkov <bglazkov@i.ua>
2023-07-24 21:10:05 +0300

    Add actual user location

--- components/Map.jsx
+++ components/Map.jsx
@@ -1,24 +1,37 @@
-import React from "react";
+import { useContext } from "react";
 import { GoogleMap, LoadScript, Marker } from "@react-google-maps/api";
+import { UserLocationContext } from "@/context/UserLocationContext";
 
 const containerStyle = {
   width: "100%",
   height: "500px",
   borderRadius: 20,
 };
-const center = {
-  lat: -3.745,
-  lng: -38.523,
-};
 
 const Map = () => {
+  const { userLocation, setUserLocation } = useContext(UserLocationContext);
+  const { lat, lng } = userLocation;
   const onLoad = (marker) => console.log("Marker: ", marker);
+
   return (
     <div>
-      <LoadScript googleMapsApiKey="">
-        <GoogleMap mapContainerStyle={containerStyle} center={center} zoom={10}>
-          <Marker onLoad={onLoad} position={center} />
-        </GoogleMap>
+      <LoadScript googleMapsApiKey={process.env.NEXT_PUBLIC_GOOGLE_API_KEY}>
+        {userLocation ? (
+          <GoogleMap
+            mapContainerStyle={containerStyle}
+            center={{ lat, lng }}
+            zoom={15}
+          >
+            <Marker
+              icon={{
+                url: "/user-location.png",
+                scaledSize: { width: 50, height: 50 },
+              }}
+              onLoad={onLoad}
+              position={{ lat, lng }}
+            />
+          </GoogleMap>
+        ) : null}
       </LoadScript>
     </div>
   );
